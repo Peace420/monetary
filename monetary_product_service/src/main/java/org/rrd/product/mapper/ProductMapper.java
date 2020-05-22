3c4780315735b1e391eb02d8f2febea41f67fb3c
Peace420 <zztwjc@aliyun.com>
2020-05-22 12:15:56 +0800

    5.22 12:00

--- monetary_product_service/src/main/java/org/rrd/product/mapper/ProductMapper.java
+++ monetary_product_service/src/main/java/org/rrd/product/mapper/ProductMapper.java
@@ -1,10 +1,17 @@
 package org.rrd.product.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.rrd.system.pojo.Product;
+
+import java.util.List;
+
 /**
  * Description: ProductMapper
  * date: 2020/5/19 14:33
  * author:
  * version: 1.0
  */
+@Mapper
 public interface ProductMapper {
+    List<Product> uplan();
 }
