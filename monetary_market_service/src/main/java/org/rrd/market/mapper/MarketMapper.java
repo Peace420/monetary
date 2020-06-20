6039a6da180cab5e4db684ad9ef7e213c930fc5e
Peace420 <zztwjc@aliyun.com>
2020-06-20 23:25:54 +0800

    6.20 commit

--- monetary_market_service/src/main/java/org/rrd/market/mapper/MarketMapper.java
+++ monetary_market_service/src/main/java/org/rrd/market/mapper/MarketMapper.java
@@ -1,27 +1,16 @@
 package org.rrd.market.mapper;
 
-<<<<<<< HEAD
-=======
+
 import org.apache.ibatis.annotations.Mapper;
 import org.rrd.system.pojo.Product;
 
 import java.util.List;
 
->>>>>>> 95524300c850d0489ab95f470a767bcd966b17d2
-/**
- * Description: MarketMapper
- * date: 2020/5/19 14:33
- * author:
- * version: 1.0
- */
-<<<<<<< HEAD
-public interface MarketMapper {
-=======
+
 @Mapper
 public interface MarketMapper {
 
     List<Product> uplan();
 
     List<Product> allProduct();
->>>>>>> 95524300c850d0489ab95f470a767bcd966b17d2
 }
